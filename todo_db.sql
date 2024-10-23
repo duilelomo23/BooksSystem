52611822a023ccbd95ce00f8c093b507c84b0838
duilelomo23 <131416644+duilelomo23@users.noreply.github.com>
2024-10-24 02:25:04 +0800

    Update todo_db.sql

--- todo_db.sql
+++ todo_db.sql
@@ -146,3 +146,41 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-09-29 22:01:30
+
+--
+-- Table structure for table `update_book_record`
+--
+
+DROP TABLE IF EXISTS `update_book_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `update_book_record` (
+  `user_book_id` int NOT NULL AUTO_INCREMENT,
+  `book_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `update_date` date DEFAULT NULL,
+  PRIMARY KEY (`user_book_id`),
+  UNIQUE KEY `user_book_id_UNIQUE` (`user_book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `update_book_record`
+--
+
+LOCK TABLES `update_book_record` WRITE;
+/*!40000 ALTER TABLE `update_book_record` DISABLE KEYS */;
+INSERT INTO `update_book_record` VALUES (1,1,3,'2024-09-21'),(2,5,1,'2024-09-21'),(3,3,3,'2024-09-21'),(4,4,1,'2024-09-21'),(5,5,2,'2024-09-21'),(6,1,1,'2024-09-21'),(7,4,3,'2024-09-22'),(8,5,3,'2024-09-22'),(9,3,2,'2024-09-22'),(10,2,3,'2024-09-23');
+/*!40000 ALTER TABLE `update_book_record` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-01 14:41:21
