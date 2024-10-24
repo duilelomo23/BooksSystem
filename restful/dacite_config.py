82631dc28efa1cc272ae780a5ad11482d2bda1fa
duilelomo23 <131416644+duilelomo23@users.noreply.github.com>
2024-10-25 02:18:27 +0800

    Update dacite_config.py

--- restful/dacite_config.py
+++ restful/dacite_config.py
@@ -8,3 +8,5 @@ def encode_datetime(dt):
     return datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
 
 config=Config(type_hooks={date: encode_date, datetime: encode_datetime})
+
+#遇到日期或時間字串，dacite 通過 type_hooks 使用定義的函數來解析並轉換，確保資料類中的 date 和 datetime 欄位能正確匹配輸入的字串格式。
